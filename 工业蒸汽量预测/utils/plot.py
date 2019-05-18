f46e2f2ca878365788ff393884d5b0ec3d259a2b
kira-youshikage <50159703+kira-youshikage@users.noreply.github.com>
2019-05-18 14:53:58 +0800

    Update plot.py

--- 工业蒸汽量预测/utils/plot.py
+++ 工业蒸汽量预测/utils/plot.py
@@ -10,11 +10,7 @@ def distribution_view(train_data, validation_data, test_data):
 	n = train_data.shape[1]
 	plt.figure(1)
 	plt_number = [5,8,0]
-	k = 0
 	for i in range(n):
-		if ((i + 1 )% 5 == 0):
-			k = 0
-		k += 1
 		plt.subplot(plt_number[0], plt_number[1], i + 1)
 		sns.distplot(train[:, i], hist = False, rug = False,label = 'train' + str(i))
 		sns.distplot(test_data[:,i], hist = False, rug = False,label = 'test')
